2ffc4e95a5dfa9705f12eda440dcd1af5f39a3df
usamataleb <mamenmasau@gmail.com>
2025-08-24 01:14:23 +0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../app.css';
+	import Footer from '../lib/components/Layout/Footer.svelte';
+	import Navbar from '../lib/components/Layout/Navbar.svelte';
+	import Topbar from '../lib/components/Layout/Topbar.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Topbar />
+	<Navbar />
+
+	<main>
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import AboutUs from "../lib/components/AboutUs.svelte";
+	import Booking from "../lib/components/Booking.svelte";
+	import Destination from "../lib/components/Destination.svelte";
+	import Hero from "../lib/components/Hero.svelte";
+	import OurService from "../lib/components/OurService.svelte";
+	import Packages from "../lib/components/Packages.svelte";
+	import Process from "../lib/components/Process.svelte";
+	import Team from "../lib/components/Team.svelte";
+	import Testimonial from "../lib/components/Testimonial.svelte";
+
+</script>
+<Hero />
+<AboutUs />
+<OurService />
+<Destination />
+<Packages />
+<Booking/>
+<Process/>
+<Team/>
+<Testimonial/>
